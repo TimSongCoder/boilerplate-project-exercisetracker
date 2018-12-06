fe671e1f4f192a6e68e6aa036c422bdfc11d1fce
Glitch (timcoder-fcc-exercisetracker) <none>
2018-12-06 06:18:12 +0000

    💖🌵 Checkpoint ./server.js:2374645/56

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-// mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/exercise-track' )
 
 app.use(cors())
 
@@ -46,3 +46,5 @@ app.use((err, req, res, next) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+app.post('/api/exercise/new-user');
